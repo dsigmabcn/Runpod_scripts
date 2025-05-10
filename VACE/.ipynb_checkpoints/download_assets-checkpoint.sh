8e3f534a6c6426e5b89723472e76726d9e32f9ed
KoalaNation <92378590+dsigmabcn@users.noreply.github.com>
2025-05-10 18:44:32 +0200

    Delete VACE/.ipynb_checkpoints/download_assets-checkpoint.sh

--- VACE/.ipynb_checkpoints/download_assets-checkpoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget -r -np -nH --cut-dirs=3 -P /workspace/ComfyUI/input https://github.com/dsigmabcn/Runpod_scripts/tree/main/VACE/inputs
-mkdir /workspace/ComfyUI/user/default/workflows/VACE
-wget -r -np -nH --cut-dirs=3 -P /workspace/ComfyUI/user/default/workflows/VACE https://github.com/dsigmabcn/Runpod_scripts/tree/main/VACE/workflows
