b12aaa76293e49d8aa34e10a02523a15d3d3fffe
KoalaNation <92378590+dsigmabcn@users.noreply.github.com>
2025-05-10 16:23:59 +0200

    Update download_assets.sh

--- VACE/download_assets.sh
+++ VACE/download_assets.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "installing workflows"
 wget -r -np -nH --cut-dirs=3 -P /workspace/ComfyUI/input https://github.com/dsigmabcn/Runpod_scripts/tree/main/VACE/inputs
+sleep 1
 echo "creating directory workflows"
 mkdir /workspace/ComfyUI/user/default/workflows/VACE
 echo "installing workflows"
 wget -r -np -nH --cut-dirs=3 -P /workspace/ComfyUI/user/default/workflows/VACE https://github.com/dsigmabcn/Runpod_scripts/tree/main/VACE/workflows
+sleep 1
