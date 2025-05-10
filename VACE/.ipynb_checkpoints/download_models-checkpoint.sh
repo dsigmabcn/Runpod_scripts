518992e8b348aa7a386a0d99b74c1315a47f0d17
dsigmabcn <david.sanchez.j81@gmail.com>
2025-05-10 07:52:58 +0000

    VACE without workflows script

---
+++ VACE/.ipynb_checkpoints/download_models-checkpoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#download text encoder
+wget -O /workspace/ComfyUI/models/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors
+#download VAE encoder
+wget -O /workspace/ComfyUI/models/vae/wan_2.1_vae.safetensors https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors
+#download WAN VACE model
+wget -O /workspace/ComfyUI/models/diffusion_models/VACE-Wan2.1-1.3B-Preview.safetensors https://huggingface.co/ali-vilab/VACE-Wan2.1-1.3B-Preview/resolve/main/diffusion_pytorch_model.safetensors
+
+
---
+++ VACE/download_assets.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -r -np -nH --cut-dirs=1 https://github.com/dsigmabcn/Runpod_scripts/blob/main/LICENSE
+
