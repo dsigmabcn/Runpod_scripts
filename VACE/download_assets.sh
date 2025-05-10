a458186932a1249e58fedd7bffe52a465139fa0f
dsigmabcn <david.sanchez.j81@gmail.com>
2025-05-10 08:31:05 +0000

    added workflows script

--- VACE/download_assets.sh
+++ VACE/download_assets.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -r -np -nH --cut-dirs=1 https://github.com/dsigmabcn/Runpod_scripts/blob/main/LICENSE
-
+wget -r -np -nH --cut-dirs=3 -P /workspace/ComfyUI/input https://github.com/dsigmabcn/Runpod_scripts/tree/main/VACE/inputs
+mkdir /workspace/ComfyUI/user/default/workflows/VACE
+wget -r -np -nH --cut-dirs=3 -P /workspace/ComfyUI/user/default/workflows/VACE https://github.com/dsigmabcn/Runpod_scripts/tree/main/VACE/workflows
