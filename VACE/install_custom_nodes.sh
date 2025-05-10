67d4829e69b1dac425205b36675ca3fe434e1db8
KoalaNation <92378590+dsigmabcn@users.noreply.github.com>
2025-05-10 16:26:47 +0200

    Update install_custom_nodes.sh

--- VACE/install_custom_nodes.sh
+++ VACE/install_custom_nodes.sh
@@ -2,19 +2,27 @@
 # clonse basic repos
 echo "clone repos"
 git -C /workspace/ComfyUI/custom_nodes clone https://github.com/kijai/ComfyUI-KJNodes.git
+sleep 1
 git -C /workspace/ComfyUI/custom_nodes clone https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite.git
+sleep 1
 git -C /workspace/ComfyUI/custom_nodes clone https://github.com/Fannovel16/comfyui_controlnet_aux.git
+sleep 1
 git -C /workspace/ComfyUI/custom_nodes clone https://github.com/kijai/ComfyUI-Florence2.git
+sleep 1
 git -C /workspace/ComfyUI/custom_nodes clone https://github.com/kijai/ComfyUI-DepthAnythingV2.git
-
+sleep 1
 #install requirements for each of the repositories
 echo "install requirements of repors"
 pip install -r /workspace/ComfyUI/custom_nodes/ComfyUI-KJNodes/requirements.txt
+sleep 1
 pip install -r /workspace/ComfyUI/custom_nodes/ComfyUI-VideoHelperSuite/requirements.txt
+sleep 1
 pip install -r /workspace/ComfyUI/custom_nodes/comfyui_controlnet_aux/requirements.txt
+sleep 1
 pip install -r /workspace/ComfyUI/custom_nodes/ComfyUI-Florence2/requirements.txt
+sleep 1
 pip install -r /workspace/ComfyUI/custom_nodes/comfyui-depthanythingv2/requirements.txt
-
+sleep 1
 
 
 
